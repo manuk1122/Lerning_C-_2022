cc0785422c4d25ee2aad4f0ea1a227700cfefbf7
ManukJr <>
2022-09-16 22:16:12 +0300

    Добавлено решение задачи 6

--- HomeWorkC#/ex6/Program.cs
+++ HomeWorkC#/ex6/Program.cs
@@ -3,3 +3,28 @@
 4 -> да
 -3 -> нет
 7 -> нет */
+while (true)
+{
+    Console.Clear();
+Console.WriteLine("Введите число для проверки на четность");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0 )
+    {
+        Console.WriteLine($"{a} -> Да");
+    }
+else
+    {
+        Console.WriteLine($"{a} - > нет ");
+    }
+Console.WriteLine("Если хотите повторить нажмите Enter");
+Console.WriteLine("если нет просто введите любой символ");
+string answ = Console.ReadLine();
+if (String.IsNullOrEmpty(answ))
+    {
+
+    }
+else 
+    {
+        break;
+    }
+}
