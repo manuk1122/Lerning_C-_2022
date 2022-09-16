b74658a81d7c7ac430b617803eb33c7a0ab60c83
ManukJr <>
2022-09-16 22:40:52 +0300

    Добавлено решение задачи 8

--- HomeWorkC#/ex8/Program.cs
+++ HomeWorkC#/ex8/Program.cs
@@ -2,3 +2,31 @@
 
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Введите целое число и программа выведет все четные числа от 1 до N");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write($"{a} -> ");
+    for (int i = 1; i<=a; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($" {i} ");
+        }
+        else
+        {}
+    }
+Console.WriteLine();
+Console.WriteLine("Если хотите повторить нажмите Enter");
+Console.WriteLine("если нет просто введите любой символ");
+string answ = Console.ReadLine();
+if (String.IsNullOrEmpty(answ))
+    {
+
+    }
+else 
+    {
+        break;
+    }
+}
