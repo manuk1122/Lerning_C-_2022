327d860cf30edcdb680ea7c497b94013623d90bd
ManukJr <>
2022-09-17 19:36:50 +0300

    оптимизирована обработка пустой строки

--- HomeWorkC#/ex2/Program.cs
+++ HomeWorkC#/ex2/Program.cs
@@ -3,6 +3,8 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
+while (true)
+{
 Console.Clear();
 Console.WriteLine("Введите 2 числа для сравнения");
 Console.WriteLine("Введите 1-е число для сравнения");
@@ -18,3 +20,9 @@ else
     {
     Console.WriteLine($"Max = {b}");
     }
+Console.WriteLine("Если хотите повторить нажмите Enter");
+Console.WriteLine("если нет просто введите любой символ");
+string answ = Console.ReadLine();
+if (!String.IsNullOrEmpty(answ))
+    break;
+        }
