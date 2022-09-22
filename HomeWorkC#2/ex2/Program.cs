1b1eb473ff18a0308fcec40bc51e1c9e7d8ed88c
ManukJr <>
2022-09-22 11:29:59 +0300

    программа оптимизирована

--- HomeWorkC#2/ex2/Program.cs
+++ HomeWorkC#2/ex2/Program.cs
@@ -9,39 +9,30 @@ while (true)
     bool flag = false;
     Console.WriteLine("Введите число для 3-й последней цифры");
     string StrNumber = Console.ReadLine();
-    int ThirdNum =0;
     flag = int.TryParse(StrNumber, out int x); // попытка конвертировать строку в число
 
     if (String.IsNullOrEmpty(StrNumber) | flag == false) // обработка на пустую строку или введный символ
         Console.WriteLine("Введена пустая строка или символ ");
 
     else
-// обработка алгоритма поиска 3-й цифры числа
+    // обработка алгоритма поиска 3-й цифры числа
     {
         int Number = int.Parse(StrNumber);
-        if (Number <= 999 & Number >= 100)
+        if (Number / 10 > 10) //  определения количества цифр в числе по целому частному(двух и более знаков)
         {
-            ThirdNum = Number % 10;
-            Console.WriteLine(ThirdNum);
+            while (Number / 10 > 99)
+            {
+                Number /= 10; // то же самое что и Number = Number / 10
+            }
+            Console.WriteLine(Number % 10);
         }
-        else if (Number <= 10000 & Number >= 1000) {ThirdNum = Number / 10 % 10;
-                Console.WriteLine(ThirdNum);}
-        else if (Number <= 99999 & Number >= 10000) {ThirdNum = Number / 100 % 10;
-                Console.WriteLine(ThirdNum);}
-        else if (Number <= 999999 & Number >= 100000) {ThirdNum = Number / 1000 % 10;
-                Console.WriteLine(ThirdNum);}
-        else if (Number <= 9999999 & Number >= 1000000) { ThirdNum = Number / 10000 % 10;
-                Console.WriteLine(ThirdNum);}
-        else if (Number <= 99999999 & Number >= 10000000) { ThirdNum = Number / 100000 % 10;
-                Console.WriteLine(ThirdNum);}
-        else if (Number <= 999999999 & Number >= 100000000) { ThirdNum = Number / 1000000 % 10;
-                Console.WriteLine(ThirdNum);}
         else
-                Console.WriteLine("Нет 3-й цифры");
+            Console.WriteLine("Нет 3-й цифры");
     }
-//Обработка повтора или выхода из алгоритма
-        Console.WriteLine(); Console.WriteLine("Если хотите повторить нажмите Enter");
-        Console.WriteLine("если нет просто введите любой символ");
-        string answ = Console.ReadLine();
-        if (!String.IsNullOrEmpty(answ)) break;
+
+    //Обработка повтора или выхода из алгоритма
+    Console.WriteLine(); Console.WriteLine("Если хотите повторить нажмите Enter");
+    Console.WriteLine("если нет просто введите любой символ");
+    string answ = Console.ReadLine();
+    if (!String.IsNullOrEmpty(answ)) break;
 }
