5aec1cf9089addd7fe54028733571fd827ae50ca
ManukJr <>
2022-10-02 17:09:44 +0300

    решение задачи 36

---
+++ HomeWorkC#5/ex2/Program.cs
@@ -0,0 +1,80 @@
+﻿/* # Задача 36: 
+## Задайте одномерный массив, заполненный 
+случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+`[3, 7, 23, 12] -> 19`
+`[-4, -6, 89, 6] -> 0` */
+// метод заполнения массива
+void FillArray(int[] collection, int min, int max)
+{
+
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(min, max);
+        index++;
+    }
+}
+// метод поиска исключений
+bool Except(string StrN)
+{
+    bool flag = false;
+    // обработка  введеной пустой строки, символа или 0
+    flag = int.TryParse(StrN, out int x);
+    if (String.IsNullOrEmpty(StrN) | flag == false)
+        Console.WriteLine("Введена пустая строка, символ или 0");
+    return flag;
+}
+// метод цветного отделения четных чисел от нечетных
+void PrintArray(int[] col)
+{
+    int index = 0;
+    Console.Write("[ ");
+    while (index < col.Length)
+    {
+       Console.ForegroundColor = index % 2 == 0 ? ConsoleColor.Red : ConsoleColor.Green;
+            Console.Write(col[index]+", ");
+            index++;
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+//метод поиска суммы элементов нечетных позиций 
+int SummOfOddPos(int[] array)
+{
+
+    int Odd = 0;
+    int count = 0;
+    while (count < array.Length)
+    {
+        Odd = count % 2 == 0 ? Odd + 0 : Odd + array[count];
+        count++;
+    }
+    return Odd;
+}
+
+while(true)
+    {
+    Console.Clear();
+    Console.WriteLine("Введите размер  массива");
+    string TempSize = Console.ReadLine();
+    Console.WriteLine("Введите левую  границу массива");
+    string TempMin = Console.ReadLine();
+    Console.WriteLine("Введите правую границу массива");
+    string TempMax = Console.ReadLine();
+    
+    if (Except(TempSize)== true & 
+        Except(TempMin)== true &
+        Except(TempMax)== true )
+    {
+    int[] ArrayOfRanum = new int[int.Parse(TempSize)];
+    FillArray(ArrayOfRanum,int.Parse(TempMin),int.Parse(TempMax));
+    PrintArray(ArrayOfRanum);
+    Console.WriteLine($"Сумма элементов нечетных позиций - > {SummOfOddPos(ArrayOfRanum)}");
+    }
+    Console.ResetColor(); 
+    Console.Write("<Enter  продолжение > <Пробел> для выхода ... ");
+    if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+        break;
+    }
