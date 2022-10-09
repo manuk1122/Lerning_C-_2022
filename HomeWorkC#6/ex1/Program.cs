7156bfbbddf90eaaa627c9844238e9698cad637f
ManukJr <>
2022-10-09 21:50:47 +0300

    решение основных задач

---
+++ HomeWorkC#6/ex1/Program.cs
@@ -0,0 +1,65 @@
+﻿/* # Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+
+`0, 7, 8, -2, -2 -> 2`
+
+`1, -7, 567, 89, 223-> 3` */
+//Заполнение массива с клавиатуры
+void FillArray(int[] collection)
+{
+    Console.WriteLine("Заполнение с клавиатуры");
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine($"Введите {index}-й элемент массива");
+        collection[index] = int.Parse(Console.ReadLine());
+        index++;
+    }
+}
+// печать массива
+void PrintArray(int[] col)
+
+{
+    int index = 0;
+    Console.Write("[ ");
+    while (index < col.Length)
+    {
+        Console.Write(col[index] + ", ");
+        index++;
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+//поиск элементов больше 0
+int NumberOfPos(int[] tempArray)
+{
+    int count = 0;
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        if (tempArray[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+while (true)
+{
+    try{
+    Console.Clear();
+    int size = new Random().Next(1, 5);
+    Console.WriteLine("Создан массив размером " + size);
+    int[] ArrayOfRanum = new int[size];
+
+    FillArray(ArrayOfRanum);
+    PrintArray(ArrayOfRanum);
+
+    Console.WriteLine("Количество элементов больше 0 = "+NumberOfPos(ArrayOfRanum));
+    
+     }
+     catch
+    {Console.WriteLine("ошибка ввода");}
+    Console.Write("<Enter  продолжение > <Пробел> для выхода ... ");
+        if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            break;
+}
---
+++ HomeWorkC#6/ex2/Program.cs
@@ -0,0 +1,51 @@
+﻿/* # Задача 43: Напишите программу, которая найдёт точку 
+пересечения двух прямых, заданных уравнениями 
+y =  k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.`
+
+`b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)` */
+
+void FillArray(double[] collection)
+{
+    Console.WriteLine("Заполнение с клавиатуры");
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine($"Введите коэффициенты");
+        collection[index] = Convert.ToDouble((Console.ReadLine()));
+        index++;
+    }
+}
+while(true)
+{
+    double[] ArrayOfRanum = new double[4];
+    Console.WriteLine("Для определения места пересечения прямых вам необходимо записать их коэффициенты");
+    Console.WriteLine("Последовательно введите коэффициенты k1,b1,k2,b2");
+try
+{
+FillArray(ArrayOfRanum); 
+
+double y = 0;
+double x = 0;
+
+Console.WriteLine($"k1 = {ArrayOfRanum[0]} ; k2 = {ArrayOfRanum[2]} ");
+Console.WriteLine($"b1 = {ArrayOfRanum[1]} ; b2 = {ArrayOfRanum[3]} ");
+if (ArrayOfRanum[0]*ArrayOfRanum[3] !=ArrayOfRanum[2]*ArrayOfRanum[1] )
+{
+ Console.WriteLine("Решение");   
+x = (ArrayOfRanum[3]-ArrayOfRanum[1])/(ArrayOfRanum[0]-ArrayOfRanum[2]);
+y = ArrayOfRanum[0]*x+ArrayOfRanum[1];
+Console.WriteLine($"Пересекутся в ({x},{y})");
+
+}
+else
+Console.WriteLine("Параллельны");
+}
+catch
+{
+Console.WriteLine("Ошибка ввода значения");
+}
+Console.Write("<Enter  продолжение > <Пробел> для выхода ... ");
+        if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            break;
+}
