7c968ae8e32f8db27e8d57acf4ca19e679297894
ManukJr <>
2022-10-10 12:50:55 +0300

    upd

---
+++ examples/seminar7/ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rowCount = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columnCount = int.Parse(Console.ReadLine());
+
+int[,] array = FillArray(rowCount, columnCount, 1, 10);
+PrintArray(array);
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] filledArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = i+j;
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(" " + inputArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+//77 3 8 1
---
+++ examples/seminar7/ex3/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// поменять местами первую и последнюю строку массива
+/* int [,] ChangeRows (int[,] array)
+{
+    int[,] changeArray = new int [array.GetLength(0), array.GetLength(1)];
+    
+    for (int i = 0; i < array.GetLength(0)-1; i++)
+    {
+        int temp = array[0,i];
+        array[0,i] = array[array.GetLength(0)-1, i];
+        array[array.GetLength(0)-1, i] = temp;
+    }
+    return changeArray;
+} */
+/* 
+FillArra(array);
+int[,] FillArra(int [,] filladArra)
+{
+
+    for (int i = 0; i < filladArra.GetLength(0); i++)
+    {
+        for (int j = 0; j < filladArra.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                (filladArra[i, j], filladArra[filladArra.GetLength(1)-1, j]) = (filladArra[filladArra.GetLength(1)-1, j], filladArra[i, j]);
+            }
+        }
+    }
+    return filladArra;
+}
+Console.WriteLine();
+PrintArray(array);
+ */
